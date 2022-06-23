94637e15d8f6c51d92e7a543735d4dce37a46dcb
JanVDB2000 <janvandenbogaert2000@gmail.com>
2022-06-23 18:13:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+const { Client } = require('discord.js'); //import discord.js
+
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] }); //create new client
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+//make sure this line is the last line
+client.login(process.env.CLIENT_TOKEN); //login bot using token
+
+
+client.on('message', msg => {
+    if (msg.content === '!Time') {
+
+        const messages = ["Time is Running", "Time Fly", "Time is money", "Time to TimeTravel", "For all time always"];
+
+        const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+
+        console.log(randomMessage)
+
+        msg.reply(randomMessage);
+    }
+});
